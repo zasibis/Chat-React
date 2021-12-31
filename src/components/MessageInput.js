bbb2536da19e1f3e9a4d764138b9b5e09effb84b
Dmytro <dsantarskyi@protonmail.com>
2021-12-31 17:15:38 +0200

    input/button

--- src/components/MessageInput.js
+++ src/components/MessageInput.js
@@ -3,9 +3,7 @@ import React from 'react';
 const MessageInput = ({addMessage}) => {
     return (
         <div className="message-input">
-            <div className="message-input-text">
-            {/* <p>dewfewf</p> */}
-            </div>
+            <input type="text" class="message-input-text" placeholder="new message" value=""></input>
             <button onClick={() => addMessage()} className="message-input-button">Send</button>
         </div>
     );
